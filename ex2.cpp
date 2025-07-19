1f0dbed8dc4c702e3d8255962e0e3aba2fd48845
fort-arch <fortn8839@gmail.com>
2025-07-19 14:10:17 +0300

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int age;
+
+string height,name;
+cout<<" Enter your name: ";
+cin>> name;
+cout<<" Enter your age: ";
+cin>> age;
+cout<<" Enter your height: ";
+cin>> height;
+
+cout<<" So, you're "<<name<<" ,who is "<<age<<" years old, "<< " and has a height of "<<height<<"meters.";
+
+
+return 0;
+}
