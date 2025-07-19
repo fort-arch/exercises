41acde3eb024de0ad79bcab28458938a8d85ce9c
fort-arch <fortn8839@gmail.com>
2025-07-19 13:17:24 +0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void greeting()
+{
+	cout<<" Hello!";
+	return ;
+	
+}
+void sum(){
+	int x=3,y=5;
+	cout <<x+y;
+	return;
+}
+void date(){
+	cout<<"Today is saturday, 19 july 2025G.C ";
+	
+}
+int main (){
+	greeting();
+	cout<<endl;
+	sum();
+	cout<<endl;
+	date();
+	return 0;
+}
